0d69d0bca3c5f089c7fb75c676ac5ebac96bbd5b
Nicolai800 <happy455420@gmail.com>
2025-01-16 18:10:23 +0100

    distinct,distinctUntilChanged,skip, filter

--- src/app/components/basic-rxjs/basic-rxjs.component.ts
+++ src/app/components/basic-rxjs/basic-rxjs.component.ts
@@ -1,10 +1,33 @@
 import { Component, inject } from '@angular/core';
-import { map, Observable, of } from 'rxjs';
+import {
+  distinct,
+  distinctUntilChanged,
+  distinctUntilKeyChanged,
+  elementAt,
+  filter,
+  from,
+  interval,
+  last,
+  map,
+  Observable,
+  of,
+  skip,
+  skipLast,
+  skipUntil,
+  take,
+  takeUntil,
+  timer,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { SubjectComponent } from './subject/subject.component';
 
-export const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export const arr: number[] = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+];
+export const arr2: number[] = [
+  1, 1, 2, 2, 3, 4, 5, 6, 1, 5, 5, 6, 6, 6, 6, 6, 7, 8,
+];
 
 export interface User {
   gender: 'male' | 'female';
@@ -23,7 +46,15 @@ export const users: { data: User[] } = {
     },
     {
       gender: 'male',
-      age: 25,
+      age: 22,
+    },
+    {
+      gender: 'male',
+      age: 15,
+    },
+    {
+      gender: 'male',
+      age: 45,
     },
     {
       gender: 'female',
@@ -52,16 +83,52 @@ export const users: { data: User[] } = {
   styleUrl: './basic-rxjs.component.scss',
 })
 export class BasicRxjsComponent {
+  sourse$ = from(arr);
+  sourse2$ = from(arr2);
+  sourse3$ = of(1, 5, 7, 6, 2, 5, 8, 4, 5, 2, 6, 8);
+  sourse4$ = interval(1000);
+  // sourseLast$ = from(arr);
+  // sourseDistinct$ = from(arr);
+
   usersObservable$ = of(users.data);
+  usersObservable2$ = from(users.data);
   usersFromjsonplaceholder$?: Observable<any>;
   http = inject(HttpClient);
-
   maleAvegeAge$?: Observable<number>;
+
   ngOnInit(): void {
     this.maleAvegeAge$ = this.getMaleAverageAge();
     this.usersFromjsonplaceholder$ = this.http.get(
       'https://jsonplaceholder.typicode.com/users'
     );
+
+    this.sourse$.pipe(take(4)).subscribe((val) => console.log('take', val));
+    this.sourse$
+      .pipe(last((el) => el % 2 === 0))
+      .subscribe((val) => console.log('last', val));
+    this.sourse$
+      .pipe(distinct())
+      .subscribe((val) => console.log('distinct', val));
+    this.sourse2$
+      .pipe(distinctUntilChanged())
+      .subscribe((val) => console.log('distinct until', val));
+    this.usersObservable2$
+      .pipe(distinctUntilKeyChanged('gender'))
+      .subscribe((val) => console.log('distinct until key', val));
+
+    this.sourse3$
+      .pipe(filter((el) => el % 2 === 0))
+      .subscribe((val) => console.log('filter', val));
+    this.sourse3$
+      .pipe(elementAt(4))
+      .subscribe((val) => console.log('index 4', val));
+    this.sourse3$.pipe(skip(4)).subscribe((val) => console.log('skip 4', val));
+    this.sourse3$
+      .pipe(skipLast(4))
+      .subscribe((val) => console.log('skip last 4', val));
+    this.sourse4$
+      .pipe(skipUntil(timer(4000)), takeUntil(timer(10000)))
+      .subscribe((val) => console.log('skip until 4sek', val));
   }
 
   getMaleAverageAge(): Observable<number> {
--- src/app/components/basic-rxjs/subject/subject.component.ts
+++ src/app/components/basic-rxjs/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -10,11 +10,28 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class SubjectComponent {
   // sourse$ = new Subject();
   sourse$ = new BehaviorSubject<number | string>('start');
+  sourseAsync$ = new AsyncSubject<number | string>();
+  sourseReplay$ = new ReplaySubject<number | string>();
 
   ngOnInit(): void {
     this.sourse$.subscribe(console.log);
     this.sourse$.next(1);
     this.sourse$.next(5);
     this.sourse$.next(7);
+
+    this.sourseAsync$.next(2);
+    this.sourseAsync$.next(7);
+    this.sourseAsync$.next('end');
+    this.sourseAsync$.next('end2');
+    this.sourseAsync$.complete();
+    this.sourseAsync$.subscribe((val) => console.log('Async', val));
+
+    this.sourseReplay$.next(2);
+    this.sourseReplay$.next(4);
+    this.sourseReplay$.next(6);
+    this.sourseReplay$.next(8);
+    this.sourseReplay$.next(10);
+    this.sourseReplay$.next('end');
+    this.sourseReplay$.subscribe(console.log);
   }
 }
