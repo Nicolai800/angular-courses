00b2bc9fd50ff7e81a249f1702533c114b3147a8
Nicolai800 <happy455420@gmail.com>
2025-01-18 20:58:17 +0100

    gitlogo, signals

--- src/app/components/signal/signal.component.ts
+++ src/app/components/signal/signal.component.ts
@@ -5,6 +5,11 @@ import {
   signal,
 } from '@angular/core';
 
+export interface TodoItem {
+  title: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-signal',
   imports: [],
@@ -14,9 +19,26 @@ import {
 })
 export class SignalComponent {
   counter = signal<number>(0);
+  showCounter = signal<boolean>(false);
+  todos = signal<TodoItem[]>([
+    { title: 'Lern Signals', done: false },
+    { title: 'Lern Signals', done: false },
+    { title: 'Lern Signals', done: false },
+    { title: 'Lern Signals', done: false },
+  ]);
+
+  changeTodoStatus(index: number) {
+    this.todos.update((val) => {
+      val[index].done = !val[index].done;
+      return val;
+    });
+  }
 
   setCounter() {
     this.counter.set(3);
+  }
+  setShowCounter() {
+    this.showCounter.set(!this.showCounter());
   }
   plusCounter() {
     this.counter.update((value) => value + 1);
@@ -24,5 +46,15 @@ export class SignalComponent {
 
   doubleCounter = computed(() => {
     return this.counter() * 2;
+  });
+  counterMiltFour = computed(() => {
+    return this.counter() * 4;
+  });
+  conditionalCount = computed(() => {
+    if (this.showCounter()) {
+      return `The count is ${this.counter()}`;
+    } else {
+      return 'HIDE';
+    }
   });
 }
